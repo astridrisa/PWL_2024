513f316deb7228c35da2ce796061fd60f8936513
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-02-21 11:12:41 +0700

    modifikasi 2

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Praktikum 1
+Route::get('/hello', function () { 
+    return 'Hello World';
+});
 
+Route::get('/world', function () { 
+    return 'World';
+});
+
+// Langkah f
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+// Langkah g
+Route::get('/about', function () {
+    $nim = '2241720250';
+    $nama = 'Astrid Risa Widiana';
+
+    return "NIM: $nim<br>Nama: $nama";
+});
+
+//Praktikum 2
+Route::get('/user/{astrid}', function ($name) { 
+    return 'Astrid Risa Widiana '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });    
+
+    Route::get('/articles/{id}', function ($id) {
+        return "Halaman Artikel dengan ID $id";
+    });
+
+
+//Praktikum 3
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Astrid Risa Widiana '.$name;
 });
